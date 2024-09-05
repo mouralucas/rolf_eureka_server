eddf7dc4d387e784990611ccb0b92b70e7952eb0
Lucas Moura <lucaspenha471@gmail.com>
2024-09-05 14:54:51 -0300

    Initial commit with basic configuration

---
+++ src/test/java/com/rolf/rolf_eureka_server/RolfEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolf.rolf_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolfEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
